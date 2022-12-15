a17dc30440c5c10e0caf67f60899cb9ef5012fc9
anju-c-a <115609401+anju-c-a@users.noreply.github.com>
2022-12-15 22:47:29 +0530

    Add files via upload

---
+++ MavenAssessment/src/main/java/org/ict/constants/AutomationConstants.java
@@ -0,0 +1,27 @@
+package org.ict.constants;
+
+public class AutomationConstants {
+	 
+			
+			public static String expusername="practice.swtesting@gmail.com";
+			public static String expPassword="123";
+			public static  String ExpectedValue=":: ICT ACADEMY OF KERALA ::";
+			
+
+			
+			
+		    public static String expname="Anju CA";
+			public static String exppassword="anju@123";
+			public static String expemail="anju@gmail.com";
+			public static String expdesignation="ARM";
+			public static String expreportingto="Demo";
+			public static String expmemberof="Staff";
+		    public static String expempid="123456";
+			public static String exppassword2="anju@123";
+			public static String expnumber="9632145780";
+			public static String exptype="Permanent";
+			public static String expaddress="Nilambur";	
+			
+		}
+
+
